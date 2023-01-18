bc9e2ccdb23179368cfb6d60baf84181fad1317c
yeeun426 <thsudkcla7@naver.com>
2023-01-18 16:22:40 +0900

    [complete] BOJ_파일정리

--- Week/week2/BOJ_파일정리_yeeun.js
+++ Week/week2/BOJ_파일정리_yeeun.js
@@ -3,8 +3,9 @@ let input = require('fs').readFileSync('예제.txt').toString().split('\n');
 let fileObj = {};
 let answer = [];
 
+// 확장자 별 개수
 for (i = 0 ; i < Number(input[0]); i++) {
-    let extension = input[i+1].split(".")[1];
+    let extension = input[i+1].trim().split(".")[1];
     fileObj[extension] ? fileObj[extension]++ : fileObj[extension] = 1;
 }
 
@@ -13,6 +14,5 @@ const extensionArr = Object.keys(fileObj).sort();
 
 for( let extension of extensionArr ) {
     answer.push(`${extension} ${fileObj[extension]}`);
-
 }
-console.log(answer.join("\n"));
+console.log(answer.join('\n'));
