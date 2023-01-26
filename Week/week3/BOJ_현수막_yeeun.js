9a17e395092e3ed204d1e3d24c4ed1b9b01f63b8
yeeun426 <thsudkcla7@naver.com>
2023-01-26 16:26:34 +0900

    [modify] BOJ_현수막

--- Week/week3/BOJ_현수막_yeeun.js
+++ Week/week3/BOJ_현수막_yeeun.js
@@ -2,13 +2,16 @@ let input = require('fs').readFileSync('예제.txt').toString().split('\n');
 let [M, N] = input[0].split(' ').map(Number);
 let words = input.slice(1).map(c => c.split(" ").map(Number));
 
-let visited = Array(N).fill(null).map(el => Array(M).fill(false));
 let cnt = 0;
 let dict = [[-1,-1],[-1,0],[-1,1],[0,1],[1,1],[1,0],[1,-1],[0,-1]];
+let visited = [];
+
+for (let i = 0; i < M; i++) {
+    visited.push(input[i+1].split(' ').fill(false));
+}
 
 for (let i = 0; i < M; i++) {
     for (let j = 0; j < N; j++) {
-        idx = j + i * N;
         // 1이고 방문하지 않은 경우
         if (words[i][j] == 1 && !visited[i][j]) {
             cnt++;
@@ -25,9 +28,7 @@ function dfs(x,y) {
         let y1 = y + dict[i][1];
         
         // 범위를 넘어가는 경우
-        if (x1 < 0 || x1 > N-1 || y1 < 0 || y1 > M-1) {
-            continue;
-        }
+        if (x1 < 0 || x1 > N-1 || y1 < 0 || y1 > M-1) continue;
         // 1이고 방문하지 않은 경우
         if (words[y1][x1] == 1 && !visited[y1][x1]) {
             visited[y1][x1] = true;
