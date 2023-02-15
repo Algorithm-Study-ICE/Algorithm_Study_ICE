4e7b3f61d6961b72c407af655af5743e0f68897b
yeeun426 <thsudkcla7@naver.com>
2023-02-16 00:31:07 +0900

    [-ing] BOJ_센티와마법의뿅망치

--- Week/week6/BOJ_센티와마법의뿅망치_yeeun.js
+++ Week/week6/BOJ_센티와마법의뿅망치_yeeun.js
@@ -1,3 +1,6 @@
+// 뭔 짓을 해도 시간초과가 뜹니다...
+// 호영이 코드 node.js로 바꿔도 시간초과고
+// 그냥 시간초과...모르겠읍니다....
 let input = require('fs').readFileSync('예제.txt').toString().trim().split('\n');
 const [N, H, T] = input[0].split(" ").map(Number);
 
@@ -6,34 +9,44 @@ for(let i = 0 ; i < N ; i++) {
     height.push(+input[i+1]);
 }
 
-const answer = [];
 let cnt = 0;
 // 가장 키 큰 거인 누군지 찾아
 // 걔 키 줄여
 // 제한 횟수도 줄여
 // 줄어든 키 다시 넣기
-// 키가 센티보다 다 작은지 확인하기
-for(let j = 0 ; j < T ; j++) {
-    let tallest = height.indexOf(Math.max(...height));
-    if((height[tallest] / 2) < 1) {
-        height[tallest] = 1;
-        // height.indexOf(Math.max(...height));
-    } else {
-        height[tallest] = parseInt(height[tallest]/2);
-    }
-}
+// 젤 큰 키가 센티보다 작은지 확인하기
+while(true) {
+    let tallest = Math.max(...height);
+    let tallIndex = height.indexOf(tallest);
 
-for(let n = 0 ; n < N; n++) {
-    if(height[n] > H) {
-        answer.push(true);
-    } else {
+    if(tallest < H) {
+        console.log("YES");
+        console.log(cnt);
+        break; 
+    }
+    if(cnt === T || tallest == 1) {
+        console.log("NO");
+        console.log(tallest);
         break;
     }
+    height[tallIndex] = parseInt(height[tallIndex]/2);
+    cnt ++;
 }
 
-if(answer.length === N)  {
-    console.log("YES");
-} else {
-    console.log("NO");
-    console.log(Math.max(...height));
-}
+// 시간초과ㅜ
+// while(tallest > H) {
+//     if(cnt === T || tallest === 1) {
+//         console.log("YES");
+//         console.log(cnt);
+//         break;
+//     } 
+//     tallIndex = height.indexOf(tallest);
+//     height[tallIndex] = parseInt(height[tallIndex]/2);
+//     tallest = Math.max(...height);
+//     cnt++;
+// }
+
+// if(Math.max(...height) >= H)  {
+//     console.log("NO");
+//     console.log(Math.max(...height));
+// }
