b0ccc3b291143a6c2778d64813806cd250bade5c
yeeun426 <thsudkcla7@naver.com>
2023-03-29 01:25:43 +0900

    [complete] BOJ_곱셈

---
+++ Week/week10/BOJ_곱셈_yeeun.js
@@ -0,0 +1,20 @@
+// BOJ 1629
+let input = require('fs').readFileSync('예제.txt').toString().trim().split(" ");
+
+const [a, b, c] = input.map(BigInt);
+
+function mult(a, b) {
+    if(b == BigInt(1)) return a % c;
+    const val = mult(a, BigInt(parseInt(b/BigInt(2))))
+
+    if(b % BigInt(2) == 0) { // b가 짝수
+        return (val * val) % c;
+    } else { // b가 홀수
+        return (val * val * a ) % c;
+    }
+}
+
+console.log(parseInt(mult(a,b)));
+
+// 지수법칙 : a^(n+m) = a^n * a^m
+// 모듈러 성질 : (a*b)%c = (a%c * b%c)%c 
